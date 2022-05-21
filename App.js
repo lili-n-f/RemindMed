f5687ab25c622f6ba8647f8ba965daa2e1fff4a0
Sabrina Correia <sabrina.correia@correo.unimet.edu.ve.com>
2022-05-20 23:26:54 -0400

    theme setup

--- App.js
+++ App.js
@@ -1,25 +1,24 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Box, NativeBaseProvider } from 'native-base';
+import { theme } from './nativeBaseConfig';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <NativeBaseProvider>
-        <Box
-          alignSelf="center"
-          bg="primary.500"
-          _text={{
-            fontSize: 'md',
-            fontWeight: 'medium',
-            color: 'warmGray.50',
-            letterSpacing: 'lg',
-          }}
-          style={{ marginBottom: 'auto', marginTop: 'auto' }}
-        >
-          Hello world
-        </Box>
-      </NativeBaseProvider>
-    </NavigationContainer>
+    <NativeBaseProvider theme={theme}>
+      <Box
+        alignSelf="center"
+        bg="primary.500"
+        _text={{
+          fontSize: 'md',
+          fontWeight: 'medium',
+          color: 'warmGray.50',
+          letterSpacing: 'lg',
+        }}
+        style={{ marginBottom: 'auto', marginTop: 'auto' }}
+      >
+        Hello world
+      </Box>
+    </NativeBaseProvider>
   );
 }
---
+++ nativeBaseConfig.js
@@ -0,0 +1,30 @@
+import { extendTheme } from 'native-base';
+
+export const theme = extendTheme({
+  colors: {
+    primary: {
+      50: '#f6f6fa',
+      100: '#eeedf5',
+      200: '#d4d1e6',
+      300: '#bab6d7',
+      400: '#867fba',
+      500: '#52489c',
+      600: '#4a418c',
+      700: '#3e3675',
+      800: '#312b5e',
+      900: '#28234c',
+    },
+    green: {
+      500: '#A2D729',
+    },
+    cyan: {
+      500: '#59C3C3',
+    },
+    blue: {
+      500: '#4062BB',
+    },
+    platinum: {
+      500: '#EBEBEB',
+    },
+  },
+});
