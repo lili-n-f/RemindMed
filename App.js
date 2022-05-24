17bb92d1eea169aa0dbccf89d93b8b60c7bf99f9
Sabrina Correia <sabrina.correia@correo.unimet.edu.ve.com>
2022-05-23 22:39:03 -0400

    welcome page

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Box, NativeBaseProvider } from 'native-base';
 import { theme } from './nativeBaseConfig';
-import NavigationBar from './src/components/NavigationBar';
+import Welcome from './src/screens/Welcome';
 
 export default function App() {
   return (
     <NavigationContainer>
       <NativeBaseProvider theme={theme}>
-        <NavigationBar />
+        <Welcome />
       </NativeBaseProvider>
     </NavigationContainer>
   );
