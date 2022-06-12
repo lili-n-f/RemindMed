8697b88670309f79250c945d1109397fea126e43
marivi <lo.maria@correo.unimet.edu.ve>
2022-06-12 11:30:00 -0400

    merge con marivi

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -25,7 +25,7 @@ export default function Login() {
   const [dataError, setDataError] = useState([]);
 
   const { user, loading } = useContext(UserContext);
-
+//zzzzz
 
 
   const handleCloseAlert = () => {
